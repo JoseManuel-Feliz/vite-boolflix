84f79ff089e16e356fe93e22bf88f76c60c6ec86
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-24 00:55:58 +0200

    added AppHeader component, removed useless files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script>
+import AppHeader from "./components/AppHeader.vue"
 export default {
   data() {
     return {
 
     }
+  },
+  components: {
+    AppHeader,
   }
 }
 </script>
 
 <template>
+  <AppHeader />
   <h1>ciao</h1>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use "./css/partials/general.scss"
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>ciaoooooo</h2>
+</template>
+
+<style scoped></style>
