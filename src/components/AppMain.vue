57cbe6bf694a3f03141bdd9ca627cfe38c4c5b76
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-24 03:36:48 +0200

    added AppMain component

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <main>
+
+        <h1>jijijiji</h1>
+    </main>
+</template>
+
+<style scoped>
+main {
+    border: 3px dashed red;
+}
+</style>
