2f665cfc5aa6c46a102f245908db34b91335f00e
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-25 08:21:43 +0200

    added streamingMedia component, moved movies loop circle to streamingmedia component

---
+++ src/components/StreamingMedia.vue
@@ -0,0 +1,69 @@
+<script>
+import { store } from '../js/store';
+
+export default {
+    name: 'StreamingMedia',
+    props: {},
+    data() {
+        return {
+            store,
+
+        }
+    },
+    computed: {
+        /*  noflag() {
+             !this.getLangFlag.includes((store.filteredMovies.original_language))
+             return this.noflag
+         },
+              */
+    },
+    methods: {
+        getLangFlag(lang) {
+            const flagsToSet =
+            {
+                en: 'gb',
+                pt: 'br',
+                zh: 'cn',
+                ja: 'jp',
+                ko: 'kr',
+                hi: 'in',
+                or: 'in',
+                cs: 'cz'
+            }
+            const isoCode = flagsToSet[lang] || lang
+            return isoCode
+        },
+        log() {
+            return console.log(store.filteredMovies)
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <p>{{ log }}</p>
+    <li v-for="movie in store.filteredMovies" :key="movie.id">
+
+        <p>
+            Titolo:
+            <span>{{ movie.title }}</span>
+        </p>,
+        <p>
+            Titolo Originale:
+            <span>{{ movie.original_title }}</span>
+        </p>
+        <p>
+            Lingua:
+            <span :class="`fi fi-${(getLangFlag(movie.original_language))}`"></span>
+        </p>
+        <p>
+            Voto:
+            <span>{{ movie.vote_average }}</span>
+        </p>
+    </li>
+
+</template>
+
+<style scoped lang="scss"></style>
