9506c59c0ab48edff5a77cf1c8009601865987f7
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-30 08:14:48 +0200

    installed bootstrap & fontawesome dependency

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from '../App.vue'
+import { createApp } from "vue";
+import App from "../App.vue";
 import "flag-icons/css/flag-icons.min.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+library.add(faStar);
 
-createApp(App).mount('#app')
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+createApp(App)
+    .component('fa-icon', FontAwesomeIcon)
+    .mount("#app")
